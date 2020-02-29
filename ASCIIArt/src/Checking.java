c95e58ae36c9095d3a574f55290ca43a18c4e3b9
s-kimyou <s-kimyou@53S500038022.it.bsd405.org>
2020-02-28 17:19:11 -0800

    (: Checkpoint 2~

---
+++ ASCIIArt/src/Checking.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Checking {
+	public static void main(String[] args) {
+		int[ ]numbers = new int[5];
+		numbers[3] = 10;
+		System.out.print(Arrays.toString(numbers));
+	}
+
+}
---
+++ ASCIIArt/src/Clone.java
@@ -0,0 +1,10 @@
+
+public class Clone {
+	private static int SIZE =4;
+	
+	public static void main(String[] args) {
+		for (int i= 0; i < SIZE; i++) {
+			System.out.println("*");
+		}
+	}
+}
